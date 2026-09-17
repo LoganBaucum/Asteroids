40ff1f1f152928740cea424cd13267fee2dba14a
LoganBaucum <LoganMBaucum@gmail.com>
2026-09-17 09:50:31 -0500

    added basic fps limit functionality

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0.0
     running = True
 
     while running:
@@ -19,6 +21,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
