bf2ee8fea71e58122b3018a3609a17fca1b9c6f5
LoganBaucum <LoganMBaucum@gmail.com>
2026-09-19 10:25:08 -0500

    add MAX_FPS and MAX_DELTA_TIME constants.

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 # Display settings
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 1920     # TODO: dynamically get display info in future.
+SCREEN_HEIGHT = 1080    # TODO: dynamically get display info in future.
+MAX_FPS = 60            # TODO: dynamically get display info in future.
+MAX_DELTA_TIME = 0.1    # seconds; avoids huge physics steps after a stall
 
 # Graphics settings
 LINE_WIDTH = 2
